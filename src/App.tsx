6db78671bd09517c9ab59c1f814102686ab18ad5
VANSH KAPOOR <vanshkapoorvk7@gmail.com>
2024-10-16 13:10:06 +0530

    check cookies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import { routes } from "./routes.js";
 import ScrollToTop from "./components/ScrollToTop.js";
 import {
@@ -11,9 +12,26 @@ function App() {
   
   const queryClient = new QueryClient()
 
+  const [domain, setDomain] = useState("")
+
+  const createCookie = () => {
+    document.cookie = `auth_token=usertoken; SameSite=None; Secure; path=/`;
+  }
+  
+  const createDomainCookie = (url) => {
+    console.log(url)
+    document.cookie = `auth_token=usertoken; SameSite=None; Secure; path=/; Domain=${url}`;
+  }
+
+
   return (
     <>
       <Router>
+      <button onClick={createCookie}>Create a cookie</button>
+      <br />
+      <input type='text' onChange={e => setDomain(e.target.value)} placeholder='enter URL' />
+      <br />
+      <button onClick={createDomainCookie}>Create a domain cookie</button>
       <ScrollToTop />
       <QueryClientProvider client={queryClient}>
       <Routes>
